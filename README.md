d1374db4eb31e07b6adb94af5f42812a66860309
狗子头林冲 <ledonwxsp@163.com>
2019-04-01 16:35:47 +0800

    基础的http服务器

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # nodejsLearning
 nodejs学习
+
+
+
+
+
+
+
+
+
---
+++ code/实例/一个基础http服务器/server.js
@@ -0,0 +1,13 @@
+// 
+var http = require('http');
+
+function handle(request, response) {
+    console.log('request received');
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write('hello world');
+    response.end();
+}
+
+http.createServer(handle).listen(8888);
+
+console.log('server has started');
