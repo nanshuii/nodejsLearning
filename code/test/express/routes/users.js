77135d841b005e90b59e1bd7419c99185d15fccb
狗子头林冲 <ledonwxsp@163.com>
2019-04-03 20:28:42 +0800

    路径优先权的例子

--- code/test/express/routes/users.js
+++ code/test/express/routes/users.js
@@ -6,15 +6,30 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-// 同一路径绑定多个路由响应函数;优先匹配前面的路径
-router.all('/:username', (req, res, next) => {
-  // res.send('all username');
-  console.log('all username');
-  next();
+// 一个例子用来
+var users = {'ledon':{
+  'name': 'ledon',
+  'god': 'is god'
+}};
+router.all('/:username', function(req, res, next) { // 检查用户是否存在
+  console.log(users[req.params.username]);
+  if (users[req.params.username]) {
+    next(); 
+  } else {
+    next(new Error(req.params.username + ' does not exist.')); }
 });
 
-router.get('/:username', (req, res, next) => {
-  res.send('username = ' + req.params.username); // 路径匹配
+router.get('/:username', function(req, res) {
+  // 用户一定存在，直接展示
+  res.send(JSON.stringify(users[req.params.username])); 
 });
+
+router.put('/:username', function(req, res) { // 修改用户信息
+  res.send('Done');
+});
+
+
+
+
 
 module.exports = router;
